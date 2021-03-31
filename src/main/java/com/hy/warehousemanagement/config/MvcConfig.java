3223f09ac79cecf214d55d8c1eb74ce57d1906e8
heyin <468611803@qq.com>
2021-03-31 15:07:01 +0800

    先提交一部分，主要是关于主页和用户信息维护的;

--- src/main/java/com/hy/warehousemanagement/config/MvcConfig.java
+++ src/main/java/com/hy/warehousemanagement/config/MvcConfig.java
@@ -17,9 +17,34 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("views/index");
 
         //控制台
-        registry.addViewController("/console").setViewName("views/home/console");
+        registry.addViewController("/home/console").setViewName("views/home/console");
+
+        //用户基本资料修改
+        registry.addViewController("/set/user/info").setViewName("views/set/user/info");
+
+        //用户密码修改
+        registry.addViewController("/set/user/password").setViewName("views/set/user/password");
+
+        //退出登录
+        registry.addViewController("/user/login").setViewName("views/user/login");
+
+        //消息中心
+        registry.addViewController("/app/message/index").setViewName("views/app/message/index");
+
+        //用户管理
+        registry.addViewController("/user/administrators/list").setViewName("views/user/administrators/list");
+
+        //角色权限管理
+        registry.addViewController("/user/administrators/role").setViewName("views/user/administrators/role");
+
+        //添加用户
+        registry.addViewController("/user/administrators/adminform").setViewName("views/user/administrators/adminform");
+
+
+
     }
 
+    //配置静态资源
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");

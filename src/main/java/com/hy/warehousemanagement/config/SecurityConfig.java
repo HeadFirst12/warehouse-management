132839b82319df08266dd4ea17895bc1f0942c21
HeadFirst12 <468611803@qq.com>
2023-06-26 16:30:12 +0800

    增加userDetailsService类的设置;

--- src/main/java/com/hy/warehousemanagement/config/SecurityConfig.java
+++ src/main/java/com/hy/warehousemanagement/config/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.hy.warehousemanagement.config;
 
-import com.hy.warehousemanagement.mapper.PersonnelManagementMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -22,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final static String NORMAL = "normal";
 
     @Resource
-    PersonnelManagementMapper personnelManagementMapper;
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -60,6 +61,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/static/**");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(getPwd());
     }
 
     @Bean
